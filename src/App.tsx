0ea376b2a096c53a46b9802a92e21a529ea06b70
Dariane Abich <darianeabich@gmail.com>
2022-05-02 10:53:32 -0300

    Configurações básicas de componentes, propriedades e do Tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import { useState } from 'react'
+interface ButtonProps {
+  text?: string;
+}
+
+function Button(props: ButtonProps) {
+  // passagem de cor:
+  // bg-violet-500 ou bg-[#8257e6]
+  return <button className='bg-[#8257e6] px-4 h-18 rounded text-violet-100 hover:bg-violet-700 transition-colors'>{props.text ?? 'Default'}</button>
+}
 
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className='flex gap-2'>
+        <Button text="Enviar" />
+        <Button text="Ok" />
+        <Button />
+    </div>
+  )
 }
 
 export default App
