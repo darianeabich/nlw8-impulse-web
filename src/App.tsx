1c5cd6376971595977db8629e1f48783b8d5b853
Dariane Abich <darianeabich@gmail.com>
2022-05-03 15:46:04 -0300

    Finalização do componente de Feedback

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,5 @@
-interface ButtonProps {
-  text?: string;
-}
+import { Widget } from './components/Widget';
 
-function Button(props: ButtonProps) {
-  // passagem de cor:
-  // bg-violet-500 ou bg-[#8257e6]
-  return <button className='bg-[#8257e6] px-4 h-18 rounded text-violet-100 hover:bg-violet-700 transition-colors'>{props.text ?? 'Default'}</button>
+export function App() {
+  return <Widget />
 }
-
-function App() {
-  return (
-    <div className='flex gap-2'>
-        <Button text="Enviar" />
-        <Button text="Ok" />
-        <Button />
-    </div>
-  )
-}
-
-export default App
